7bbf74b41b3d96c815924ce08f71e427bc18efd5
johnray <johnray@localhost>
2020-03-28 19:57:09 -0400

    initial check in of sns hello world example

---
+++ Terraform/SNS-HelloWorld/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  default = "AKIAIE6PXH3MJOCQSKDA"
+}
+
+variable "secret_key" {
+  default = "yUUb17GagfseXeJnTj1yEnOXKI4ns0LCV7fwB5oC"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
