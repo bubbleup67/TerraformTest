17ead4ec825ab6b1fd9a248646d4dac6ba445144
johnray <johnray@localhost>
2020-03-29 16:58:40 -0400

    correct credentials mix up

--- Terraform/SNS-HelloWorld/sns.tf
+++ Terraform/SNS-HelloWorld/sns.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+  region = "us-east-2"
+  shared_credentials_file = "/Users/johnray/.aws/credentials"
+  profile = "bubbleup"
 }
 
 resource "aws_sns_topic" "JR" {
